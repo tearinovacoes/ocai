f9de9b3a4f904418b1c8643c643ed4fb51fe862f
Juliano Bittencourt <juliano.bittencourt@gmail.com>
2017-08-04 11:51:04 -0300

    Corrigindo problemas na ordenção
    Corrigido problema na ordenação das categorias que gerava um render errado.

--- lib.php
+++ lib.php
@@ -16,7 +16,13 @@
           subitem on response.subitem_id=subitem.subitem_id
         group by
           subitem.org_culture
-        order by org_culture asc;";
+        order by case org_culture
+                      when 'Clan' then 1
+                      when 'Adhocracy' then 2
+                      when 'Market' then 3
+                      when 'Hierarchy' then 4
+                      else 5
+                  end";
 
       $results["all"] = ORM::for_table('response')->raw_query($str)->find_array();
 
@@ -34,9 +40,16 @@
         inner join item on item.item_id=subitem.item_id
         group by
           item.item_id, subitem.org_culture
-        order by item.item_id;";
+        order by item.item_id,
+              case subitem.org_culture when 'Clan' then 1
+                  when 'Adhocracy' then 2
+                  when 'Market' then 3
+                  when 'Hierarchy' then 4
+                  else 5
+              end;";
 
       $results["by_dimension"] = ORM::for_table('response')->raw_query($str)->find_array();
+
       return $results;
   }
 
