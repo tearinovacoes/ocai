75f444035d6fc417c79e5912cc530ac94b372f7d
Juliano Bittencourt <juliano.bittencourt@gmail.com>
2021-02-26 00:09:51 -0300

    Update class.formr.php

--- formr/class.formr.php
+++ formr/class.formr.php
@@ -350,10 +350,10 @@ class Formr {
 		for($i = 1; $i < $length; $i = strlen($string)) {
 			
 			# grap a random character
-			$r = $chars{rand(0, $chars_length)};
+			$r = $chars[rand(0, $chars_length)];
 			
 			# make sure the same characters don't appear next to each other
-			if($r != $string{$i - 1}) $string .= $r;
+			if($r != $string[$i - 1]) $string .= $r;
 		}
 		
 		return $string;
